610683d2af9a433cb40feb88a08fd0f126f2bd16
Bishoy Farouk <bftawfik@gmail.com>
2021-12-12 11:25:10 +0200

    use StartNow in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,11 @@
+import StartNow from '../../Components/StartNow/StartNow'
+
+import * as classes from "./Home.module.scss";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div className={classes.Home}>
+    <StartNow />
+  </div>;
 };
 
 export default Home;
