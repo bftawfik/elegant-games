9b7898baa2f9c70b5145396848641bf48adc3478
Bishoy Farouk <bftawfik@gmail.com>
2021-12-13 16:33:46 +0200

    fix backdrop fadeout animation

--- src/Components/Backdrop/Backdrop.tsx
+++ src/Components/Backdrop/Backdrop.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import classes from "./Backdrop.module.scss";
 
 type backdropProps = {
@@ -7,12 +8,24 @@ type backdropProps = {
 };
 
 const Backdrop = ({ zndx, opacity, backgroundColor }: backdropProps) => {
+  const [fadeOut, setFadeOut] = useState(false);
+  const [hide, setHide] = useState(false);
   return (
     <button
-      className={classes.Backdrop}
+      className={[
+        classes.Backdrop,
+        fadeOut ? classes.fadeOut : undefined,
+        hide ? classes.hide : undefined,
+      ].join(" ")}
       style={{
         zIndex: zndx,
         backgroundColor: `rgba(${backgroundColor}, ${opacity})`,
+      }}
+      onClick={() => {
+        setFadeOut(true);
+        setTimeout(() => {
+          setHide(true);
+        }, 1000);
       }}
     />
   );
