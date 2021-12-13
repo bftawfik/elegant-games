a6502b683d141fd3bb1a15d913847d21aa406ef8
Bishoy Farouk <bftawfik@gmail.com>
2021-12-13 16:24:03 +0200

    use Backdrop in home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import StartNow from '../../Components/StartNow/StartNow'
+import Backdrop from "../../Components/Backdrop/Backdrop";
+import StartNow from "../../Components/StartNow/StartNow";
 
-import  classes from "./Home.module.scss";
+import classes from "./Home.module.scss";
 
 const Home = () => {
-  return <div className={classes.Home}>
-    <StartNow />
-  </div>;
+  return (
+    <div className={classes.Home}>
+      <Backdrop zndx={100} opacity={0.5} />
+      <StartNow />
+    </div>
+  );
 };
 
 export default Home;
