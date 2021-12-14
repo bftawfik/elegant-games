28907b1b34e5c4a55cebada99277800bda70f11c
Bishoy Farouk <bftawfik@gmail.com>
2021-12-14 11:19:03 +0200

    clean home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,9 @@
-import Backdrop from "../../Components/Backdrop/Backdrop";
-import StartNow from "../../Components/StartNow/StartNow";
-
 import classes from "./Home.module.scss";
 
 const Home = () => {
   return (
     <div className={classes.Home}>
-      <Backdrop zndx={100} opacity={0.5} />
-      <StartNow />
+      Home
     </div>
   );
 };
