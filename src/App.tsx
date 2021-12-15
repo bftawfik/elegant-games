d1a4f1ab2f3cb4b21eb3a48427ec06f24ce1cd54
Bishoy Farouk <bftawfik@gmail.com>
2021-12-15 15:11:58 +0200

    add game data list to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
 import type {
   typeSocialIcons,
   typeInternalLinks,
-  typeGameListData,
   typeTermsData,
   typePrivacyData,
 } from "./Types";
@@ -65,79 +64,47 @@ const internalLinks: typeInternalLinks = [
   },
 ];
 
-const listData: typeGameListData = [
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
-  {
-    name: "hello",
-    gameId: "5",
-    imgUrl:
-      "https://www.ourfastcdn.com/elegantgames/mygames/21.Cars/HTML5-Game/icon-256.png",
-  },
+const gamesData = [
+  "01.Splishy_Fish",
+  "02.FunnyAnimals",
+  "03.ClassicMatch3",
+  "04.TrafficRacer",
+  "05.BlocksSuperMatch3",
+  "06.HappyHalloweenMatch3",
+  "07.HappyChefBubbleShooter",
+  "08.SeaAnimals",
+  "09.Christmas_Candy",
+  "10.LollipopsMatch3",
+  "11.SpeedRacer",
+  "12.CandyMatch3",
+  "13.SmilesMatch3",
+  "14.ValentinesMatch3",
+  "15.ChristmasMatch3",
+  "16.AnimalsCrashMatch3",
+  "17.HalloweenMatch3",
+  "18.ScaryRun",
+  "19.Billiards",
+  "20.CrazyMatch3",
+  "21.Cars",
+  "22.MonsterMatch3",
+  "23.SweetMatch3",
+  "24.CrazyCar",
+  "25.SummerMatch3",
+  "26.BubbleShooter",
+  "27.FunnyFacesMatch3",
+  "28.SpaceMatch3",
+  "29.MathGameForKids",
+  "30.TrackRacer",
+  "31.GoofRunner",
+  "32.ChristmasGifts",
+  "33.ChristmasBubbles",
+  "34.StickPanda",
+  "35.ChristmasBalls",
+  "36.RoadRacer",
+  "37.JewelsMatch",
+  "38.PopsBilliards",
+  "39.MiniRacer",
+  "40.FrogSuperBubbles",
 ];
 
 const termsData: typeTermsData = [
@@ -163,7 +130,13 @@ function App() {
   return (
     <div className="App">
       <StaticDataProvider
-        value={{ socialIcons, internalLinks, listData, termsData, privacyData }}
+        value={{
+          socialIcons,
+          internalLinks,
+          termsData,
+          privacyData,
+          gamesData,
+        }}
       >
         <Router />
       </StaticDataProvider>
