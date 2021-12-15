ec91f686af27c2fb3aa9f1382aa907184b83342f
Bishoy Farouk <bftawfik@gmail.com>
2021-12-15 12:57:22 +0200

    add base SingleGame

--- src/Pages/SingleGame/SingleGame.tsx
+++ src/Pages/SingleGame/SingleGame.tsx
@@ -1,5 +1,14 @@
+import classes from "./SingleGame.module.scss";
+
 const SingleGame = () => {
-  return <div>Single Game</div>;
+  return (
+    <div className={classes.SingleGame}> 
+      <iframe
+        src="https://elegant.games/"
+        title="W3Schools Free Online Web Tutorials"
+      ></iframe>
+    </div>
+  );
 };
 
 export default SingleGame;
