4b686c9d3a4338dc06a912b702ca7b063b4e9482
Bishoy Farouk <bftawfik@gmail.com>
2021-12-15 14:09:59 +0200

    use terms and privacy pages in router

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -5,6 +5,9 @@ import Footer from "../Components/Footer/Footer";
 
 import Home from "../Pages/Home/Home";
 import SingleGame from "../Pages/SingleGame/SingleGame";
+import Terms from "../Pages/Terms/Terms";
+import Privacy from "../Pages/Privacy/Privacy";
+
 import Login from "../Pages/Login/Login";
 import Unsubscribe from "../Pages/Unsubscribe/Unsubscribe";
 import Missing from "../Pages/Missing/Missing";
@@ -17,8 +20,8 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/games/" element={<Home />} />
-        <Route path="/privacy/" element={<Home />} />
-        <Route path="/terms/" element={<Home />} />
+        <Route path="/privacy/" element={<Privacy />} />
+        <Route path="/terms/" element={<Terms />} />
         <Route path="/games/:gameId" element={<SingleGame />} />
         <Route path="/cancel" element={<Unsubscribe />} />
         <Route path="*" element={<Missing />} />
