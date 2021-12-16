3a95eff66032be705e418565d00c35ee8f93bd9c
Bishoy Farouk <bftawfik@gmail.com>
2021-12-16 14:51:13 +0200

    add extractParams helper

--- src/Services/Helpers.ts
+++ src/Services/Helpers.ts
@@ -1,4 +1,21 @@
 let percentToHex: Function = (p: number): string =>
   Math.round(p * 255).toString(16);
 
-export { percentToHex };
+const getQueryParams =
+  (queryParams: URLSearchParams): Function =>
+  (singleParam: string): string =>
+    queryParams.get(singleParam) || "";
+
+const extractParams = (
+  queryParams: URLSearchParams,
+  ...params: string[]
+): undefined | string | string[] => {
+  const getQueryParamsAsString = getQueryParams(queryParams);
+  return params?.length === 0
+    ? undefined
+    : params.length === 1
+    ? getQueryParamsAsString(params[0])
+    : params.map((singleParam) => getQueryParamsAsString(singleParam));
+};
+
+export { percentToHex, extractParams };
