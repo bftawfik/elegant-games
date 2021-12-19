4844942ab0684765e127c5e3b5587781e366dcfb
Bishoy Farouk <bftawfik@gmail.com>
2021-12-19 16:00:24 +0200

    add RegisterCard to login page

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,5 +1,37 @@
+import { Link } from "react-router-dom";
+
+import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
+import RegisterCard from "../../Components/RegisterCard/RegisterCard";
+
+import classes from "./Login.module.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <FulscrnWrpr
+      className={classes.Login}
+      containerClassName={classes.container}
+    >
+      <div className={classes.contents}>
+        <h2>تسجيل الدخول</h2>
+        <RegisterCard
+          title={"registerCardTitle.1"}
+          telPrefix={"telPrefix"}
+          telPattern={"telPattern"}
+          telMaxLength={7}
+          telPlaceHolder={"telPlaceHolder.1"}
+          submitLabel={"registerButtonLabel.1"}
+          providers={[]}
+          provider={undefined}
+          onSelectChange={undefined}
+          terms={null}
+          tel={"tel"}
+          // onChange={setTel}
+          // onSubmit={verifyTel}
+          // enriched={enriched}
+        />
+      </div>
+    </FulscrnWrpr>
+  );
 };
 
 export default Login;
