09b7f5ad575774d584353e86a9a89d5463995421
Bishoy Farouk <bftawfik@gmail.com>
2021-12-19 14:14:37 +0200

    add the search params to url

--- src/Components/GameThumbBox/GameThumbBox.tsx
+++ src/Components/GameThumbBox/GameThumbBox.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 import type { typeSingleGameData } from "../../Types";
 
 import classes from "./GameThumbBox.module.scss";
 
 const GameThumbBox = ({ gameId }: typeSingleGameData) => {
+  const {search} = useLocation();
+
+  
   return (
     <div className={classes.GameThumbBox}>
-      <Link to={`/games/${gameId}`}>
+      <Link to={`/games/${gameId}${search}`}>
         <img
           src={`https://www.ourfastcdn.com/elegantgames/mygames/${gameId}/HTML5-Game/icon-256.png`}
           alt={gameId}
