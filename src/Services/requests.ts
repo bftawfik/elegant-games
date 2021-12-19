d2f91e6bef1bf91cda3c025854a5370935a785f7
Bishoy Farouk <bftawfik@gmail.com>
2021-12-19 15:00:18 +0200

    add the login request dummy

--- src/Services/requests.ts
+++ src/Services/requests.ts
@@ -2,6 +2,7 @@ const protocol: string = "http";
 const server: string = "94.237.109.240";
 const port: string = "4200";
 
+
 const postIsSubscribed = async (token: string) =>
   await fetch(`${protocol}://${server}:${port}/tpay/isSubscribed`, {
     method: "POST",
@@ -15,5 +16,26 @@ const postIsSubscribed = async (token: string) =>
     referrerPolicy: "no-referrer",
     body: JSON.stringify({ token: token }),
   });
+
+// const server: string = "94.237.109.240";
+// const port: string = "4000";
+
+// const postIsSubscribed = async (token: string) =>
+//   await fetch(`${protocol}://${server}:${port}/center/login`, {
+//     method: "POST",
+//     mode: "cors",
+//     cache: "no-cache",
+//     credentials: "same-origin",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     redirect: "follow",
+//     referrerPolicy: "no-referrer",
+//     body: JSON.stringify({
+//       msisdn: "+201000603308",
+//       service: "KindGames",
+//       language: 1,
+//     }),
+//   });
 
 export { postIsSubscribed };
