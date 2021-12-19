20baccc43edcc462b9e93767077b4305bb1e7641
Bishoy Farouk <bftawfik@gmail.com>
2021-12-19 14:17:14 +0200

    use Redirect component in the SingleGame

--- src/Pages/SingleGame/SingleGame.tsx
+++ src/Pages/SingleGame/SingleGame.tsx
@@ -1,16 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { useParams, useNavigate} from "react-router-dom";
+
+import Redirect from "../../Components/Redirect/Redirect";
+
+import type { typeAppProviderValue } from "../../Types";
+
+import { AppDataContext } from "../../Components/AppDataProvider/AppDataProvider";
 
 import classes from "./SingleGame.module.scss";
 
 const SingleGame = () => {
-  let { gameId } = useParams(); 
-  return (
+  let { gameId } = useParams();
+  const { user }: typeAppProviderValue = useContext(AppDataContext);
+  const navigate = useNavigate();
+  if (!user) {
+    navigate("/");
+  }
+
+  return user ? (
     <div className={classes.SingleGame}>
       <iframe
         src={`https://www.ourfastcdn.com/elegantgames/mygames/${gameId}/HTML5-Game/index.html`}
         title={gameId}
       />
     </div>
+  ) : (
+    <Redirect externalUrl="http://www.google.com" />
   );
 };
 
