eb9cdb2a0b4f83d853d30125ed432375626fb932
Bishoy Farouk <bftawfik@gmail.com>
2021-12-21 15:32:15 +0200

    fix login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,29 +1,76 @@
+import { useState } from "react";
 import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
 import RegisterCard from "../../Components/RegisterCard/RegisterCard";
+import type { typeCountryData } from "../../Types";
+
+import { postCenterLogin } from "../../Services/requests";
 
 import classes from "./Login.module.scss";
 
 const Login = () => {
+  const [selectedCountry, setselectedCountry] = useState<
+    undefined | typeCountryData
+  >(undefined);
+  const [tel, setTel] = useState<string>("");
+
+  const changeCountry = (option: any) => {
+    setselectedCountry(option.value);
+  };
+
+  const changeTel = (newTel: string) => {
+    setTel(newTel);
+  };
+
+  const onSubmitHandler = async (e: any) => {
+    e.preventDefault();
+    const res = await postCenterLogin(
+      `${selectedCountry?.dial_code}${tel}`,
+      "ElegantGames",
+      1
+    );
+    const resJson = await res.json();
+    console.log(resJson);
+
+    // if (statusCode === 200) {
+    //   const { decryptedObj, isSubscribed } = data;
+    //   if (isSubscribed) {
+    //     setIsSubscribed(true);
+    //     setUser({
+    //       ...decryptedObj,
+    //     });
+    //   } else {
+    //     setIsSubscribed(false);
+    //   }
+    // }
+  };
+
+  // const checkToken = async (token: string) => {
+  //   const res = await postCenterLogin(token);
+  //   const { statusCode, data } = await res.json();
+  //   if (statusCode === 200) {
+  //     const { decryptedObj, isSubscribed } = data;
+  //     if (isSubscribed) {
+  //       setIsSubscribed(true);
+  //       setUser({
+  //         ...decryptedObj,
+  //       });
+  //     } else {
+  //       setIsSubscribed(false);
+  //     }
+  //   }
+  // };
+
   return (
     <FulscrnWrpr
       className={classes.Login}
       containerClassName={classes.container}
     >
       <RegisterCard
-        title={"تسجيل الدخول"}
-        telPrefix={"02"}
-        telPattern={"telPattern"}
-        telMaxLength={7}
-        telPlaceHolder={"telPlaceHolder.1"}
-        submitLabel={"sub"}
-        providers={[]}
-        provider={undefined}
-        onSelectChange={undefined}
-        terms={null}
-        tel={"0127778885"}
-        // onChange={setTel}
-        // onSubmit={verifyTel}
-        // enriched={enriched}
+        tel={tel}
+        changeCountryHandler={changeCountry}
+        selectedCountry={selectedCountry}
+        onChange={changeTel}
+        onSubmit={onSubmitHandler}
       />
     </FulscrnWrpr>
   );
