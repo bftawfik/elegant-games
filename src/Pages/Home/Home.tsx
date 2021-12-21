fedc384acf70f6d8fc114ade50e6aea6299dbadf
Bishoy Farouk <bftawfik@gmail.com>
2021-12-21 17:06:20 +0200

    add navigate to home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,14 +1,24 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
 import GamesGrid from "../../Components/GamesGrid/GamesGrid";
 import GameThumbBox from "../../Components/GameThumbBox/GameThumbBox";
 
+import type { typeAppProviderValue } from "../../Types";
+
+import { AppDataContext } from "../../Components/AppDataProvider/AppDataProvider";
+
 import classes from "./Home.module.scss";
 
 const Home = () => {
-  return (
+  const { userData }: typeAppProviderValue = useContext(AppDataContext);
+  
+  return userData?.isSubscribed ? (
     <FulscrnWrpr className={classes.Home}>
       <GamesGrid card={GameThumbBox} />
     </FulscrnWrpr>
+  ) : (
+    <Navigate to="/login" replace={true} />
   );
 };
 
