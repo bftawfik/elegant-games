293918cdc339dc9b8d91783dbbf67fbc916fcdb0
Bishoy Farouk <bftawfik@gmail.com>
2021-12-21 15:15:39 +0200

    add postCenterLogin

--- src/Services/requests.ts
+++ src/Services/requests.ts
@@ -1,7 +1,7 @@
 const protocol: string = "http";
 const server: string = "94.237.109.240";
 const port: string = "4200";
-
+const loginPort: string = "4000";
 
 const postIsSubscribed = async (token: string) =>
   await fetch(`${protocol}://${server}:${port}/tpay/isSubscribed`, {
@@ -17,25 +17,26 @@ const postIsSubscribed = async (token: string) =>
     body: JSON.stringify({ token: token }),
   });
 
-// const server: string = "94.237.109.240";
-// const port: string = "4000";
+const postCenterLogin = async (
+  msisdn: string,
+  serviceName: string,
+  language: number
+) =>
+  await fetch(`${protocol}://${server}:${loginPort}/center/login`, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify({
+      msisdn: msisdn,
+      service: serviceName,
+      language: language,
+    }),
+  });
 
-// const postIsSubscribed = async (token: string) =>
-//   await fetch(`${protocol}://${server}:${port}/center/login`, {
-//     method: "POST",
-//     mode: "cors",
-//     cache: "no-cache",
-//     credentials: "same-origin",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     redirect: "follow",
-//     referrerPolicy: "no-referrer",
-//     body: JSON.stringify({
-//       msisdn: "+201000603308",
-//       service: "KindGames",
-//       language: 1,
-//     }),
-//   });
-
-export { postIsSubscribed };
+export { postIsSubscribed, postCenterLogin };
