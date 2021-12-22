defbbf8e3d4c048416152e413edfed2ac567569d
Bishoy Farouk <bftawfik@gmail.com>
2021-12-22 16:54:23 +0200

    read language from provider

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 
+import { AppDataContext } from "../AppDataProvider/AppDataProvider";
+
 import Backdrop from "../Backdrop/Backdrop";
 
-import { defaults } from "../../Mocks";
+import { defaults, languagesData } from "../../Mocks";
+
+import type { typeAppProviderValue } from "../../Types";
 
 import classes from "./Header.module.scss";
 
@@ -16,18 +20,23 @@ const Header = () => {
   const navigate = useNavigate();
   const { t, i18n } = useTranslation();
   const { productId } = defaults;
+  const i18Trans = languagesData.map((lang) => i18n.getFixedT(lang));
   const headerCases = {
     SHOW_WITHOUT_ANIMATION: "SHOW_WITHOUT_ANIMATION",
     HIDE_WITHOUT_ANIMATION: "HIDE_WITHOUT_ANIMATION",
     SHOW_WITH_ANIMATION: "SHOW_WITH_ANIMATION",
     HIDE_WITH_ANIMATION: "HIDE_WITH_ANIMATION",
   };
-
+  const { language }: typeAppProviderValue = useContext(AppDataContext);
   const [showHeader, setShowHeader] = useState<string>(
     location?.pathname === "/"
       ? headerCases.SHOW_WITHOUT_ANIMATION
       : headerCases.HIDE_WITHOUT_ANIMATION
   );
+
+  const switchLang = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
 
   return (
     <>
@@ -47,34 +56,43 @@ const Header = () => {
         <h1>{i18n.getFixedT("en")(`products.${productId}`)}</h1>
         <h2>{t("headerData.header1.1")}</h2>
         <h2>{t("headerData.header2.1")}</h2>
-        <button
-          onClick={() => {
-            if (location?.pathname === "/") {
-              if (
-                showHeader === headerCases.HIDE_WITH_ANIMATION ||
-                showHeader === headerCases.HIDE_WITHOUT_ANIMATION
-              ) {
-                setShowHeader(headerCases.SHOW_WITH_ANIMATION);
-              } else {
-                setShowHeader(headerCases.HIDE_WITH_ANIMATION);
-              }
-            } else {
-              if (
-                showHeader === headerCases.HIDE_WITHOUT_ANIMATION ||
-                showHeader === headerCases.HIDE_WITH_ANIMATION
-              ) {
-                navigate("/");
-              } else {
-                setShowHeader(headerCases.SHOW_WITH_ANIMATION);
-              }
-            }
-          }}
-        >
-          <span className={classes.msg}>{t("headerData.startBtn.1")}</span>
-          <span className={classes.icon}>
+        <div className={classes.buttonsContainer}>
+          {/* <span className={classes.icon}>
             <FontAwesomeIcon icon={faHome} />
-          </span>
-        </button>
+          </span> */}
+          {languagesData.map((lang, ndx) => (
+            <button
+              key={ndx}
+              className={lang === language ? classes.selected : undefined}
+              onClick={() => {
+                switchLang(lang);
+                if (location?.pathname === "/") {
+                  if (
+                    showHeader === headerCases.HIDE_WITH_ANIMATION ||
+                    showHeader === headerCases.HIDE_WITHOUT_ANIMATION
+                  ) {
+                    setShowHeader(headerCases.SHOW_WITH_ANIMATION);
+                  } else {
+                    setShowHeader(headerCases.HIDE_WITH_ANIMATION);
+                  }
+                } else {
+                  if (
+                    showHeader === headerCases.HIDE_WITHOUT_ANIMATION ||
+                    showHeader === headerCases.HIDE_WITH_ANIMATION
+                  ) {
+                    // navigate("/");
+                  } else {
+                    setShowHeader(headerCases.SHOW_WITH_ANIMATION);
+                  }
+                }
+              }}
+            >
+              <span className={classes.msg}>
+                {i18Trans[ndx]("headerData.startBtn.1")}
+              </span>
+            </button>
+          ))}
+        </div>
       </div>
     </>
   );
