87255610f8df4f0188ef3a097ed58dd958321ae7
Bishoy Farouk <bftawfik@gmail.com>
2021-12-22 15:08:24 +0200

    move terms data to n18i

--- src/Pages/Terms/Terms.tsx
+++ src/Pages/Terms/Terms.tsx
@@ -1,13 +1,20 @@
 import { useContext } from "react";
+import { useTranslation } from "react-i18next";
 
 import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
 import { AppDataContext } from "../../Components/AppDataProvider/AppDataProvider";
 
 import type { typeAppProviderValue } from "../../Types";
 
+import { defaults } from "../../Mocks";
+
 import classes from "./Terms.module.scss";
 
 const Terms = () => {
+  const { t, i18n } = useTranslation();
+  const en = i18n.getFixedT("en");
+  const { productId } = defaults;
+
   const { termsData }: typeAppProviderValue = useContext(AppDataContext);
   return (
     <FulscrnWrpr
@@ -15,10 +22,12 @@ const Terms = () => {
       containerClassName={classes.container}
     >
       <div className={classes.contents}>
-        <h2>اهلا بكم في ElegantGames</h2>
+        <h2>{`${t("termsData.header1.1")} ${en(`products.${productId}`)}`}</h2>
         <ul>
           {termsData &&
-            termsData.map((termLine, ndx) => <li key={ndx}>{termLine}</li>)}
+            termsData.map((termId, ndx) => (
+              <li key={ndx}>{t(`termsData.list1.item${ndx}.${termId}`)} </li>
+            ))}
         </ul>
       </div>
     </FulscrnWrpr>
